deb17285ab92dc2c5e2fd0b08667c0d273f4bc41
castrokimaru <castrokimaru.com>
2025-10-20 12:18:56 +0300

    configuring plugin-react

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+export const viteConfig = {
+  server: {
+    port: 3000,
+    open: true,
+  },
+}
